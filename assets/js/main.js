e95088ade0bce502f8c28744a70cbd63eaca994d
dtaylor89 <dlonzo.t@gmail.com>
2023-11-04 01:41:38 -0400

    lamda function fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -157,3 +157,14 @@
 		});
 
 })(jQuery);
+
+
+const counter = document.querySelector(".counter-number");
+async function updateCounter() {
+    let response = await fetch(
+        "https://n5musrdl5pvkym2bbqwx4lyora0dppdy.lambda-url.us-east-1.on.aws/"
+    );
+    let data = await response.json();
+    counter.innerHTML = `👀 Views: ${data}`;
+}
+updateCounter();
